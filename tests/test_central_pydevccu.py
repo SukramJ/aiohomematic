f2dc2fd24a7af26d95916ed078df4d9da8989afb
SukramJ <sukramj@icloud.com>
2025-01-05 15:51:08 +0100

    ignore Falmot

--- tests/test_central_pydevccu.py
+++ tests/test_central_pydevccu.py
@@ -118,7 +118,7 @@ async def test_central_full(central_unit_full) -> None:
     ) as fptr:
         fptr.write(orjson.dumps(addresses, option=orjson.OPT_INDENT_2 | orjson.OPT_NON_STR_KEYS))
 
-    assert usage_types[DataPointUsage.NO_CREATE] == 4092
+    assert usage_types[DataPointUsage.NO_CREATE] == 4091
     assert usage_types[DataPointUsage.CDP_PRIMARY] == 261
     assert usage_types[DataPointUsage.DATA_POINT] == 3688
     assert usage_types[DataPointUsage.CDP_VISIBLE] == 133

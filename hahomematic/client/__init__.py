9150e547154d7144d8ce93af566161718883f957
SukramJ <sukramj@icloud.com>
2025-01-08 09:08:29 +0100

    Remove default

--- hahomematic/client/__init__.py
+++ hahomematic/client/__init__.py
@@ -1082,14 +1082,14 @@ class ClientCCU(Client):
         """Get single system variable from CCU / Homegear."""
         return await self._json_rpc_client.get_system_variable(name=name)
 
-    @service(re_raise=False, no_raise_return=None)
+    @service(re_raise=False)
     async def get_all_system_variables(
         self, markers: tuple[DescriptionMarker | str, ...]
     ) -> tuple[SystemVariableData, ...] | None:
         """Get all system variables from CCU."""
         return await self._json_rpc_client.get_all_system_variables(markers=markers)
 
-    @service(re_raise=False, no_raise_return=None)
+    @service(re_raise=False)
     async def get_all_programs(self, markers: tuple[DescriptionMarker | str, ...]) -> tuple[ProgramData, ...]:
         """Get all programs, if available."""
         return await self._json_rpc_client.get_all_programs(markers=markers)
@@ -1407,7 +1407,7 @@ class ClientHomegear(Client):
         """Get single system variable from CCU / Homegear."""
         return await self._proxy.getSystemVariable(name)
 
-    @service(re_raise=False, no_raise_return=None)
+    @service(re_raise=False)
     async def get_all_system_variables(
         self, markers: tuple[DescriptionMarker | str, ...]
     ) -> tuple[SystemVariableData, ...] | None:
@@ -1418,7 +1418,7 @@ class ClientHomegear(Client):
                 variables.append(SystemVariableData(vid=name, legacy_name=name, value=value))
         return tuple(variables)
 
-    @service(re_raise=False, no_raise_return=None)
+    @service(re_raise=False)
     async def get_all_programs(self, markers: tuple[DescriptionMarker | str, ...]) -> tuple[ProgramData, ...] | None:
         """Get all programs, if available."""
         return ()

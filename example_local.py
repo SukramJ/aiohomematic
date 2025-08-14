575748d7426050ee624deb05c6eb4541621234d8
SukramJ <sukramj@icloud.com>
2025-08-14 14:36:16 +0200

    Ren

--- example_local.py
+++ example_local.py
@@ -448,7 +448,7 @@ class Example:
             username=CCU_USERNAME,
             password=CCU_PASSWORD,
             central_id="1234",
-            storage_folder="homematicip_local",
+            storage_folder="aiohomematic_storage",
             interface_configs={interface_config},
             default_callback_port=48888,
         ).create_central()

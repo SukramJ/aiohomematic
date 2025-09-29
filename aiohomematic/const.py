e3ab2ef8137e1a8fba54e229176d6c0d7a976372
SukramJ <sukramj@icloud.com>
2025-09-29 12:29:01 +0200

    Add CuXD parameters CMD_RETL and CMD_RETS to ignore list

--- aiohomematic/const.py
+++ aiohomematic/const.py
@@ -19,7 +19,7 @@ import sys
 from types import MappingProxyType
 from typing import Any, Final, NamedTuple, Required, TypeAlias, TypedDict
 
-VERSION: Final = "2025.9.7"
+VERSION: Final = "2025.9.8"
 
 # Detect test speedup mode via environment
 _TEST_SPEEDUP: Final = (

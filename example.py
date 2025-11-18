1e040064619fe1a27c0df08a2a478e36bb559759
SukramJ <sukramj@icloud.com>
2025-11-18 23:07:42 +0100

    Update example.py

--- example.py
+++ example.py
@@ -16,9 +16,9 @@ from aiohomematic.model.custom import validate_custom_data_point_definition
 logging.basicConfig(level=logging.DEBUG)
 _LOGGER = logging.getLogger(__name__)
 
-CCU_HOST = "192.168.1.173"
-CCU_USERNAME = "Admin"
-CCU_PASSWORD = ""
+CCU_HOST = "XXX"
+CCU_USERNAME = "xxx"
+CCU_PASSWORD = "xxx"
 
 
 class Example:
@@ -33,9 +33,25 @@ class Example:
         self.central = None
 
     def _systemcallback(self, event: BackendSystemEventData) -> None:
-        """Handle backend system events."""
-        if event.system_event == const.BackendSystemEvent.DEVICES_CREATED:
+        """Handle system events from the backend."""
+        self.got_devices = True
+        if (
+            event.system_event == const.BackendSystemEvent.NEW_DEVICES
+            and event.data
+            and event.data.get("device_descriptions")
+            and len(event.data["device_descriptions"]) > 0
+        ):
             self.got_devices = True
+            return
+        if (
+            event.system_event == const.BackendSystemEvent.DEVICES_CREATED
+            and event.data
+            and event.data.get("new_data_points")
+            and len(event.data["new_data_points"]) > 0
+        ):
+            if len(event.data["new_data_points"]) > 1:
+                self.got_devices = True
+            return
 
     async def example_run(self):
         """Process the example."""
@@ -46,16 +62,21 @@ class Example:
                 interface=const.Interface.HMIP_RF,
                 port=2010,
             ),
+            # InterfaceConfig(
+            #     central_name=central_name,
+            #     interface=const.Interface.BIDCOS_RF,
+            #     port=2001,
+            # ),
+            # InterfaceConfig(
+            #     central_name=central_name,
+            #     interface=const.Interface.VIRTUAL_DEVICES,
+            #     port=9292,
+            #     remote_path="/groups",
+            # ),
             InterfaceConfig(
                 central_name=central_name,
-                interface=const.Interface.BIDCOS_RF,
-                port=2001,
-            ),
-            InterfaceConfig(
-                central_name=central_name,
-                interface=const.Interface.VIRTUAL_DEVICES,
-                port=9292,
-                remote_path="/groups",
+                interface=const.Interface.CUXD,
+                port=8701,
             ),
         }
         self.central = CentralConfig(
@@ -64,13 +85,19 @@ class Example:
             username=CCU_USERNAME,
             password=CCU_PASSWORD,
             central_id="1234",
+            storage_directory="aiohomematic_storage",
             interface_configs=interface_configs,
+            callback_port_xml_rpc=54323,
+            optional_settings=(const.OptionalSettings.ENABLE_LINKED_ENTITY_CLIMATE_ACTIVITY,),
         ).create_central()
 
         # For testing we set a short INIT_TIMEOUT
         const.INIT_TIMEOUT = 10
-        # Add callbacks to handle the events and see what happens on the system.
-        self.central.event_bus.subscribe(event_type=BackendSystemEventData, handler=self._systemcallback)
+        # Subscribe to backend system events via EventBus
+        self.central.event_bus.subscribe(
+            event_type=BackendSystemEventData,
+            handler=self._systemcallback,
+        )
 
         await self.central.start()
         while not self.got_devices and self.SLEEPCOUNTER < 20:

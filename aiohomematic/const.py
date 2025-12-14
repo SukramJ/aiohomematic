84766fbc21a0acde83e8a156d200727bf82e14e9
SukramJ <sukramj@icloud.com>
2025-12-14 17:44:14 +0100

    Fix firmware data not updating after refresh (#2574)

--- aiohomematic/const.py
+++ aiohomematic/const.py
@@ -19,7 +19,7 @@ import sys
 from types import MappingProxyType
 from typing import Any, Final, NamedTuple, Required, TypedDict
 
-VERSION: Final = "2025.12.26"
+VERSION: Final = "2025.12.27"
 
 # Detect test speedup mode via environment
 _TEST_SPEEDUP: Final = (

3f1da919330c97a31d0ed7df659fa9563a9e8b6a
SukramJ <sukramj@icloud.com>
2026-02-23 17:08:21 +0100

    Use JSON-RPC for HmIP-RF install mode

--- aiohomematic/const.py
+++ aiohomematic/const.py
@@ -21,7 +21,7 @@ from typing import Any, Final, NamedTuple, Required, TypeAlias, TypedDict
 
 from pydantic import BaseModel, ConfigDict
 
-VERSION: Final = "2026.2.24"
+VERSION: Final = "2026.2.25"
 
 # Detect test speedup mode via environment
 _TEST_SPEEDUP: Final = (

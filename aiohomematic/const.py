e5aa98ebbafc47f7a5111094585d29393600877a
SukramJ <sukramj@icloud.com>
2026-03-30 15:19:27 +0200

    Quittable flag based on trigger datapoint

--- aiohomematic/const.py
+++ aiohomematic/const.py
@@ -19,7 +19,7 @@ from typing import Any, Final, NamedTuple, Required, TypeAlias, TypedDict
 
 from pydantic import BaseModel, ConfigDict
 
-VERSION: Final = "2026.3.20"
+VERSION: Final = "2026.3.21"
 
 # Detect test speedup mode via environment
 _TEST_SPEEDUP: Final = (
